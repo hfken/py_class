d7975b96c7e5bad2978cb26a2e5882f440d7a791
hfken <1607226047@qq.com>
2025-06-07 11:14:09 +0800

    new

--- file/file.py
+++ file/file.py
@@ -1,3 +1,7 @@
-with open(r'text.txt','rt',encoding='utf-8') as f:
-    
+with open(r'file\text.txt','r+',encoding='utf-8') as f:
+    f.writelines('nihao')
+    f.write('nihao')
+    f.write('3')
+    f.seek(0)
+    f.write('3')
 
