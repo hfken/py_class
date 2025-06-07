5746db8bffb1a44c360ec37073a9d3cd94e89bf6
hfken <1607226047@qq.com>
2025-06-07 18:25:59 +0800

    new

---
+++ numpy/numpys.py
@@ -0,0 +1,5 @@
+import numpy as np
+my=np.array([1,2,4,5])
+print(my.ndim)
+print(my.size)
+print(my.shape)
--- text.py
+++ text.py
@@ -1,2 +1,11 @@
-i=map(lambda x:x**2,(1,2,3))
-for t in i:print(t,end=' ')
+dict1 = {1: '周一',2: '周二', 3: '周三',4: '周四',5: '周五',6: '周六'}
+for i in range(1,5):
+    for j in range(i+1,5):
+        print('{}考X'.format(dict1[i]),end=' ')
+        print('{}考Y'.format(dict1[j]),end=' ')
+        print('{}考Z'.format(dict1[5]))
+for i in range(1,6):
+    for j in range(i+1,6):
+        print('{}考X'.format(dict1[i]),end=' ')
+        print('{}考Y'.format(dict1[j]),end=' ')
+        print('{}考Z'.format(dict1[6]))
